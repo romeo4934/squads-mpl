b3fc70201b629765d071f367b5a5e49edf2f36a5
Antoine Herzog <antoine.herzog@hey.com>
2024-07-25 21:37:37 +0100

    making sure the mode is passed correctly

--- programs/squads-mpl/src/lib.rs
+++ programs/squads-mpl/src/lib.rs
@@ -242,11 +242,16 @@ pub mod squads_mpl {
     /// instructions, whereas authorities 1 or greater refer to a vault,
     /// upgrade authority, or other.
     pub fn create_transaction(ctx: Context<CreateTransaction>, authority_index: u32, mode: ApprovalMode,) -> Result<()> {
-        if let ApprovalMode::ApprovalByPrimaryMember = mode {
-        if ctx.accounts.multisig.primary_member.is_none() {
-            return err!(MsError::NoPrimaryMemberSpecified);
+        match mode {
+            ApprovalMode::ApprovalByPrimaryMember | ApprovalMode::ApprovalByMultisig => {}
+            _ => return err!(MsError::InvalidApprovalMode),
         }
-    }
+
+        if let ApprovalMode::ApprovalByPrimaryMember = mode {
+            if ctx.accounts.multisig.primary_member.is_none() {
+                return err!(MsError::NoPrimaryMemberSpecified);
+            }
+        }
         let ms = &mut ctx.accounts.multisig;
         let authority_bump = match authority_index {
             1.. => {
